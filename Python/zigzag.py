626fd2e55335cc81ac2bfaa9fe7510438e70b9ce
Aakash Semwal <semwalaakash24@gmail.com>
2025-08-30 15:09:32 +0530

    Learned Exception Handling

---
+++ Python/zigzag.py
@@ -0,0 +1,20 @@
+import time, sys
+indent = 0 # How many spaces to indent
+indent_increasing = True # Whether the indentation is increasing or not
+
+try:
+    while True:
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10th of a second.
+
+        if indent_increasing: # Increase the number of spaces
+            indent += 1
+            if indent == 20: # Change direction
+                indent_increasing = False
+        else: # Decrease the number of spaces
+            indent -= 1
+            if indent == 0:
+                indent_increasing = True
+except KeyboardInterrupt:
+    sys.exit()
